35aa1d66da2ef662d4ddbe5ccadef2572912bbf8
selistym <selistym@protonmail.com>
2019-08-20 12:25:31 -0700

    re-init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { NameList, sendAnalyticsPing } from "./helpers";
 
 import {
     unstable_LowPriority,
@@ -20,52 +19,8 @@ export default function App() {
 
     return (
         <div className="App">
-            <SearchBox onChange={handleChange} />
-            <NameList searchValue={searchValue} />
-        </div>
-    );
-}
-
-function SearchBox(props) {
-    const [inputValue, setInputValue] = useState("");
-
-    const handleChange = event => {
-        const value = event.target.value;
-        const onChange = props.onChange;
-        if(ENABLE_CONCURRENT_AND_SCHEDULED){
-
-            setInputValue(value);
-            unstable_next(function () {
-                onChange(value);
-            });
             
-            sendDeferredAnalyticsPing(value);
-        }else{
-            setInputValue(value);
-            onChange(value);
-            sendAnalyticsPing(value);
-        }
-
-    };
-
-    return (
-        <div className="clearfix">
-            <input
-                type="text"
-                className="input"
-                value={inputValue}
-                onChange={handleChange}
-                placeholder="Filter 🔍"
-            />
         </div>
     );
 }
 
-
-function sendDeferredAnalyticsPing(value) {
-    unstable_runWithPriority(unstable_LowPriority, function () {
-        unstable_scheduleCallback(function () {
-            sendAnalyticsPing(value);
-        });
-    });
-}
